be5b0be8594519da8c21bbcd593f5f07847db244
Wenlan Hua <alex_hua@outlook.com>
2018-04-26 15:19:38 +0200

    1.0 open a window

--- start/main.cpp
+++ start/main.cpp
@@ -1,10 +1,36 @@
-#include "common.h"
+//
+//  main.cpp
+//  OpenGLDemo
+//
+//  Created by 919575700@qq.com on 16/4/24.
+//  Copyright © 2016年 Jiangxh. All rights reserved.
+//
+#include <GLUT/GLUT.h> // GLUT窗口库
 
-bool isRunning = true;
-
-int main(int argc, char* argv[])
+// 自定义一个绘制函数
+void display()
 {
-    cout << "Hello OpenGL!" << endl;
+    // 清空
+    glClear(GL_COLOR_BUFFER_BIT);
+    // 开始绘制一个多边形
+    glBegin(GL_POLYGON);
+    // 多边形的顶点（三角形）
+    glVertex2f(-0.5, -0.5);
+    glVertex2f(0, 0.5);
+    glVertex2f(0.5, -0.5);
+    //绘制结束
+    glEnd();
+    glFlush();
+}
 
-    return 0;
+// 程序入口
+int main(int argc, char ** argv)
+{
+    //窗口初始化
+    glutInit(&argc, argv);
+    //创建一个名为Demo的窗口
+    glutCreateWindow("Demo");
+    //绘图函数
+    glutDisplayFunc(display);
+    glutMainLoop();
 }
