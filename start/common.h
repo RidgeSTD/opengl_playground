5fee479b1e843c8261c15d360601a6cd74bfb89c
Wenlan Hua <alex_hua@outlook.com>
2018-04-26 15:06:51 +0200

    add start project, firstly maually setup env

---
+++ start/common.h
@@ -0,0 +1,22 @@
+#ifndef _COMMON_H
+#define _COMMON_H
+
+/*=======STANDARD TEMPLATE LIBRARY=======*/
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <memory>
+#include <ctime>
+#include <vector>
+
+using namespace std;
+
+/*=======GLU/GLEW=======*/
+#include <OpenGL/gl.h> 
+#include <OpenGL/glu.h> 
+#include <GLUT/glut.h>
+
+/*=======CLASS DEFINITIONS=======*/
+
+
+#endif
---
+++ start/main.cpp
@@ -0,0 +1,10 @@
+#include "common.h"
+
+bool isRunning = true;
+
+int main(int argc, char* argv[])
+{
+    cout << "Hello OpenGL!" << endl;
+
+    return 0;
+}
