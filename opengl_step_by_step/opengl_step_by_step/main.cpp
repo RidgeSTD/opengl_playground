e05532ba6e1c42507e057030fec6ab8d906a7a0c
alex <alex_hua@outlook.com>
2018-06-03 00:52:23 +0200

    tutorial 02

--- opengl_step_by_step/opengl_step_by_step/main.cpp
+++ opengl_step_by_step/opengl_step_by_step/main.cpp
@@ -1,14 +1,50 @@
-#include <iostream>
+#include <stdio.h>
+#include "Dependencies/glew/glew.h"
 #include "Dependencies/freeglut/freeglut.h"
+#include "includes/ogldev_math_3d.h"
+
+GLuint VBO;
 
 /**
 * 渲染回调函数
 */
-void RenderScenceCB() {
+static void RenderScenceCB() {
 	// 清空颜色缓存
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	// 开启顶点属性
+	glEnableVertexAttribArray(0);
+	// 绑定GL_ARRAY_BUFFER缓冲器
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	// 告诉管线怎样解析bufer中的数据
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+	// 开始绘制几何图形(绘制一个点)
+	glDrawArrays(GL_POINTS, 0, 1);
+
+	//  禁用顶点数据
+	glDisableVertexAttribArray(0);
+
 	// 交换前后缓存
 	glutSwapBuffers();
+}
+
+/**
+* 创建顶点缓冲器
+*/
+static void CreateVertexBuffer()
+{
+	// 创建含有一个顶点的顶点数组
+	Vector3f Vertices[1];
+	// 将点置于屏幕中央
+	Vertices[0] = Vector3f(0.0f, 0.0f, 0.0f);
+
+	// 创建缓冲器
+	glGenBuffers(1, &VBO);
+	// 绑定GL_ARRAY_BUFFER缓冲器
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	// 绑定顶点数据
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
 }
 
 /**
@@ -25,13 +61,24 @@ int main(int argc, char ** argv) {
 	// 窗口设置
 	glutInitWindowSize(480, 320);      // 窗口尺寸
 	glutInitWindowPosition(100, 100);  // 窗口位置
-	glutCreateWindow("Tutorial 01");   // 窗口标题
+	glutCreateWindow("Tutorial 02");   // 窗口标题
 
 									   // 开始渲染
 	glutDisplayFunc(RenderScenceCB);
 
+	// 检查GLEW是否就绪，必须要在GLUT初始化之后！
+	GLenum res = glewInit();
+	if (res != GLEW_OK) {
+		fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+		return 1;
+	}
+
 	// 缓存清空后的颜色值
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+	// 创建顶点缓冲器
+	CreateVertexBuffer();
+
 
 	// 通知开始GLUT的内部循环
 	glutMainLoop();
