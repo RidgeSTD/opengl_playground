6853eb93d37cdcb5dae2eea6516330a9722a7041
alex <alex_hua@outlook.com>
2018-06-10 14:53:38 +0200

    tutorial 05

--- opengl_step_by_step/opengl_step_by_step/main2.cpp
+++ opengl_step_by_step/opengl_step_by_step/main2.cpp
@@ -1,18 +1,16 @@
-#ifndef WIN32
-#define WIN32
-#endif
-
 #include <stdio.h>
 #include <string.h>
+
+#include <math.h>
 #include "Dependencies/glew/glew.h"
 #include "Dependencies/freeglut/freeglut.h"
 
-#include "includes/ogldev_util.h" //这里要添加作者的工具类用于读取文本文件
+#include "includes/ogldev_util.h"
 #include "includes/ogldev_math_3d.h"
 
 GLuint VBO;
+GLuint gScaleLocation; // 位置中间变量
 
-// 定义要读取的顶点着色器脚本和片断着色器脚本的文件名，作为文件读取路径（这样的话shader.vs和shader.fs文件要放到工程的根目录下，保证下面定义的是这两个文件的文件路径）
 const char* pVSFileName = "shader.vs";
 const char* pFSFileName = "shader.fs";
 
@@ -20,11 +18,16 @@ static void RenderSceneCB()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 
+	// 维护一个不断慢慢增大的静态浮点数
+	static float Scale = 0.0f;
+	Scale += 0.001f;
+	// 将值传递给shader
+	glUniform1f(gScaleLocation, fabsf(sinf(Scale)));
+
 	glEnableVertexAttribArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
-	// 依然还是绘制一个三角形
 	glDrawArrays(GL_TRIANGLES, 0, 3);
 
 	glDisableVertexAttribArray(0);
@@ -32,10 +35,11 @@ static void RenderSceneCB()
 	glutSwapBuffers();
 }
 
-
 static void InitializeGlutCallbacks()
 {
 	glutDisplayFunc(RenderSceneCB);
+	// 将渲染回调注册为全局闲置回调
+	glutIdleFunc(RenderSceneCB);
 }
 
 static void CreateVertexBuffer()
@@ -50,26 +54,21 @@ static void CreateVertexBuffer()
 	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
 }
 
-// 使用shader文本编译shader对象，并绑定shader都想到着色器程序中
 static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
 {
-	// 根据shader类型参数定义两个shader对象
 	GLuint ShaderObj = glCreateShader(ShaderType);
-	// 检查是否定义成功
+
 	if (ShaderObj == 0) {
 		fprintf(stderr, "Error creating shader type %d\n", ShaderType);
-		exit(0);
+		exit(1);
 	}
 
-	// 定义shader的代码源
 	const GLchar* p[1];
 	p[0] = pShaderText;
 	GLint Lengths[1];
 	Lengths[0] = strlen(pShaderText);
 	glShaderSource(ShaderObj, 1, p, Lengths);
-	glCompileShader(ShaderObj);// 编译shader对象
-
-							   // 检查和shader相关的错误
+	glCompileShader(ShaderObj);
 	GLint success;
 	glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);
 	if (!success) {
@@ -79,38 +78,33 @@ static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum Shad
 		exit(1);
 	}
 
-	// 将编译好的shader对象绑定到program object程序对象上
 	glAttachShader(ShaderProgram, ShaderObj);
 }
 
-// 编译着色器函数
-static void CompileShaders()
-{
-	// 创建着色器程序
+static void CompileShaders() {
 	GLuint ShaderProgram = glCreateProgram();
-	// 检查是否创建成功
+
 	if (ShaderProgram == 0) {
 		fprintf(stderr, "Error creating shader program\n");
 		exit(1);
 	}
 
-	// 存储着色器文本的字符串缓冲
 	string vs, fs;
-	// 分别读取着色器文件中的文本到字符串缓冲区
+
 	if (!ReadFile(pVSFileName, vs)) {
 		exit(1);
 	};
+
 	if (!ReadFile(pFSFileName, fs)) {
 		exit(1);
 	};
 
-	// 添加顶点着色器和片段着色器
 	AddShader(ShaderProgram, vs.c_str(), GL_VERTEX_SHADER);
 	AddShader(ShaderProgram, fs.c_str(), GL_FRAGMENT_SHADER);
 
-	// 链接shader着色器程序，并检查程序相关错误
 	GLint Success = 0;
 	GLchar ErrorLog[1024] = { 0 };
+
 	glLinkProgram(ShaderProgram);
 	glGetProgramiv(ShaderProgram, GL_LINK_STATUS, &Success);
 	if (Success == 0) {
@@ -119,7 +113,6 @@ static void CompileShaders()
 		exit(1);
 	}
 
-	// 检查验证在当前的管线状态程序是否可以被执行
 	glValidateProgram(ShaderProgram);
 	glGetProgramiv(ShaderProgram, GL_VALIDATE_STATUS, &Success);
 	if (!Success) {
@@ -128,22 +121,25 @@ static void CompileShaders()
 		exit(1);
 	}
 
-	// 设置到管线声明中来使用上面成功建立的shader程序
 	glUseProgram(ShaderProgram);
+
+	// 查询获取一致变量的位置
+	gScaleLocation = glGetUniformLocation(ShaderProgram, "gScale");
+	// 检查错误
+	assert(gScaleLocation != 0xFFFFFFFF);
 }
 
-// 主函数
 int main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowSize(1024, 768);
 	glutInitWindowPosition(100, 100);
-	glutCreateWindow("Tutorial 04");
+	glutCreateWindow("Tutorial 05");
 
 	InitializeGlutCallbacks();
 
-	// 必须在glut初始化后！
+	// Must be done after glut is initialized!
 	GLenum res = glewInit();
 	if (res != GLEW_OK) {
 		fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
@@ -156,7 +152,6 @@ int main(int argc, char** argv)
 
 	CreateVertexBuffer();
 
-	// 编译着色器
 	CompileShaders();
 
 	glutMainLoop();
