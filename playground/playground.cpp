3bddf4b335a5643ca8f5e0fdd19181e6561764de
winlandiano <alex_hua@outlook.com>
2018-11-14 16:56:12 +0100

    mission 05 texture cube

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -11,6 +11,7 @@ GLFWwindow* window;
 using namespace glm;
 
 #include "common/shader.hpp"
+#include "common/texture.hpp"
 
 // 定义立方体顶点坐标
 static const GLfloat g_vertex_buffer_data[] = {
@@ -52,56 +53,44 @@ static const GLfloat g_vertex_buffer_data[] = {
 	1.0f,-1.0f, 1.0f
 };
 
-static const GLfloat triangle_vertex_buffer_data[] = {
-	-1.5f,-1.5f,0.0f, // triangle 1 : begin
-	1.5f,-1.5f, 0.0f,
-	1.5f, 1.5f, 0.0f, // triangle 1 : end
-};
-
-static const GLfloat g_color_buffer_data_triangle[] = {
-	-1.5f,-1.5f,0.0f, // triangle 1 : begin
-	1.5f,-1.5f, 0.0f,
-	1.5f, 1.5f, 0.0f, // triangle 1 : end
-};
-
-// One color for each vertex. They were generated randomly.
-static GLfloat g_color_buffer_data[] = {
-	0.583f,  0.771f,  0.014f,
-	0.609f,  0.115f,  0.436f,
-	0.327f,  0.483f,  0.844f,
-	0.822f,  0.569f,  0.201f,
-	0.435f,  0.602f,  0.223f,
-	0.310f,  0.747f,  0.185f,
-	0.597f,  0.770f,  0.761f,
-	0.559f,  0.436f,  0.730f,
-	0.359f,  0.583f,  0.152f,
-	0.483f,  0.596f,  0.789f,
-	0.559f,  0.861f,  0.639f,
-	0.195f,  0.548f,  0.859f,
-	0.014f,  0.184f,  0.576f,
-	0.771f,  0.328f,  0.970f,
-	0.406f,  0.615f,  0.116f,
-	0.676f,  0.977f,  0.133f,
-	0.971f,  0.572f,  0.833f,
-	0.140f,  0.616f,  0.489f,
-	0.997f,  0.513f,  0.064f,
-	0.945f,  0.719f,  0.592f,
-	0.543f,  0.021f,  0.978f,
-	0.279f,  0.317f,  0.505f,
-	0.167f,  0.620f,  0.077f,
-	0.347f,  0.857f,  0.137f,
-	0.055f,  0.953f,  0.042f,
-	0.714f,  0.505f,  0.345f,
-	0.783f,  0.290f,  0.734f,
-	0.722f,  0.645f,  0.174f,
-	0.302f,  0.455f,  0.848f,
-	0.225f,  0.587f,  0.040f,
-	0.517f,  0.713f,  0.338f,
-	0.053f,  0.959f,  0.120f,
-	0.393f,  0.621f,  0.362f,
-	0.673f,  0.211f,  0.457f,
-	0.820f,  0.883f,  0.371f,
-	0.982f,  0.099f,  0.879f
+// 纹理位置
+static const GLfloat g_uv_buffer_data[] = {
+	0.000059f, 1.0f - 0.000004f,
+	0.000103f, 1.0f - 0.336048f,
+	0.335973f, 1.0f - 0.335903f,
+	1.000023f, 1.0f - 0.000013f,
+	0.667979f, 1.0f - 0.335851f,
+	0.999958f, 1.0f - 0.336064f,
+	0.667979f, 1.0f - 0.335851f,
+	0.336024f, 1.0f - 0.671877f,
+	0.667969f, 1.0f - 0.671889f,
+	1.000023f, 1.0f - 0.000013f,
+	0.668104f, 1.0f - 0.000013f,
+	0.667979f, 1.0f - 0.335851f,
+	0.000059f, 1.0f - 0.000004f,
+	0.335973f, 1.0f - 0.335903f,
+	0.336098f, 1.0f - 0.000071f,
+	0.667979f, 1.0f - 0.335851f,
+	0.335973f, 1.0f - 0.335903f,
+	0.336024f, 1.0f - 0.671877f,
+	1.000004f, 1.0f - 0.671847f,
+	0.999958f, 1.0f - 0.336064f,
+	0.667979f, 1.0f - 0.335851f,
+	0.668104f, 1.0f - 0.000013f,
+	0.335973f, 1.0f - 0.335903f,
+	0.667979f, 1.0f - 0.335851f,
+	0.335973f, 1.0f - 0.335903f,
+	0.668104f, 1.0f - 0.000013f,
+	0.336098f, 1.0f - 0.000071f,
+	0.000103f, 1.0f - 0.336048f,
+	0.000004f, 1.0f - 0.671870f,
+	0.336024f, 1.0f - 0.671877f,
+	0.000103f, 1.0f - 0.336048f,
+	0.336024f, 1.0f - 0.671877f,
+	0.335973f, 1.0f - 0.335903f,
+	0.667969f, 1.0f - 0.671889f,
+	1.000004f, 1.0f - 0.671847f,
+	0.667979f, 1.0f - 0.335851f
 };
 
 int main(void)
@@ -162,10 +151,12 @@ int main(void)
 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
-	GLuint vertexbuffer_triangle;
-	glGenBuffers(1, &vertexbuffer_triangle);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer_triangle);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle_vertex_buffer_data), triangle_vertex_buffer_data, GL_STATIC_DRAW);
+	// 加载纹理
+	GLuint texture = loadDDS("uvtemplate.DDS");
+	GLuint uvBuffer;
+	glGenBuffers(1, &uvBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, uvBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_uv_buffer_data), g_uv_buffer_data, GL_STATIC_DRAW);
 
 	GLuint programID = LoadShaders("SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader");
 
@@ -173,25 +164,15 @@ int main(void)
 	// 物体放在原点不动
 	mat4 model = mat4(1.0f);
 	mat4 view = glm::lookAt(
-		glm::vec3(3, 4, 5), // Camera is at (4,3,3), in World Space
+		glm::vec3(4, 3, 3), // Camera is at (4,3,3), in World Space
 		glm::vec3(0, 0, 0), // and looks at the origin
 		glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
 	);
-	mat4 project = glm::perspective(glm::radians(90.0f), 4.0f/3.0f, 0.1f, 20.0f);
+	mat4 project = glm::perspective(glm::radians(90.0f), 4.0f/3.0f, 0.1f, 100.0f);
 	mat4 MVP = project * view * model;
 
-	mat4 model2 = mat4(1, 0, 0, 1,
-						0, 1, 0, 0,
-						0, 0, 1, 3,
-						0, 0, 0, 1);
-	mat4 MVP2 = project * view * model2;
-
 	GLuint mvp_id = glGetUniformLocation(programID, "MVP");
-
-	// 加入颜色
-	GLuint colorBuffer;
-	glGenBuffers(1, &colorBuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
+	GLuint sampler_id = glGetUniformLocation(programID, "textureSampler");
 
 	do {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -204,34 +185,24 @@ int main(void)
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
 
 		glEnableVertexAttribArray(1);
-		glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
-		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+		glBindBuffer(GL_ARRAY_BUFFER, uvBuffer);
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
 
 		glUniformMatrix4fv(mvp_id, 1, GL_FALSE, &MVP[0][0]);
+		
+		// appint the texture sampler
+		// 默认情况0号就是激活的，这里只是显式的调用一次
+		// 纹理单元的编号可以通过着色器访问到
+		// 可以看见这里设定了uniform告知着色器使用那个Texture Unit
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		// 纹理的采样处理
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glGenerateMipmap(GL_TEXTURE_2D);
+		glUniform1i(sampler_id, 0);
 
 		glDrawArrays(GL_TRIANGLES, 0, 3 * 12);
-		glDisableVertexAttribArray(0);
-
-		// draw triangle
-		glEnableVertexAttribArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer_triangle);
-		g_color_buffer_data[64] += 0.002f;
-		if (g_color_buffer_data[64] >=1.0f)
-		{
-			g_color_buffer_data[64] -= 1.0f;
-		}
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-
-		glEnableVertexAttribArray(1);
-		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer_triangle);
-		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-
-		glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
-
-		glUniformMatrix4fv(mvp_id, 1, GL_FALSE, &MVP2[0][0]);
-
-		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glDisableVertexAttribArray(0);
 
 		// Swap buffers
